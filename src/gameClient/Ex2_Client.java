ecf2ae75e4c7351d0ac7cb334a4712fccf0bebed
yakovdabush1 <73227745+yakovdabush1@users.noreply.github.com>
2020-12-21 22:24:59 +0200

    Starting a project

--- src/gameClient/Ex2_Client.java
+++ src/gameClient/Ex2_Client.java
@@ -19,10 +19,10 @@ public class Ex2_Client implements Runnable{
 		Thread client = new Thread(new Ex2_Client());
 		client.start();
 	}
-	
+
 	@Override
 	public void run() {
-		int scenario_num = 11;
+		int scenario_num = 1;
 		game_service game = Game_Server_Ex2.getServer(scenario_num); // you have [0,23] games
 	//	int id = 999;
 	//	game.login(id);
@@ -30,12 +30,12 @@ public class Ex2_Client implements Runnable{
 		String pks = game.getPokemons();
 		directed_weighted_graph gg = game.getJava_Graph_Not_to_be_used();
 		init(game);
-		
+
 		game.startGame();
 		_win.setTitle("Ex2 - OOP: (NONE trivial Solution) "+game.toString());
 		int ind=0;
 		long dt=100;
-		
+
 		while(game.isRunning()) {
 			moveAgants(game, gg);
 			try {
@@ -52,7 +52,7 @@ public class Ex2_Client implements Runnable{
 		System.out.println(res);
 		System.exit(0);
 	}
-	/** 
+	/**
 	 * Moves each of the agents along the edge,
 	 * in case the agent is on a node the next destination (next edge) is chosen (randomly).
 	 * @param game
@@ -109,7 +109,7 @@ public class Ex2_Client implements Runnable{
 		_win.setSize(1000, 700);
 		_win.update(_ar);
 
-	
+
 		_win.show();
 		String info = game.toString();
 		JSONObject line;
@@ -127,7 +127,7 @@ public class Ex2_Client implements Runnable{
 				CL_Pokemon c = cl_fs.get(ind);
 				int nn = c.get_edge().getDest();
 				if(c.getType()<0 ) {nn = c.get_edge().getSrc();}
-				
+
 				game.addAgent(nn);
 			}
 		}
